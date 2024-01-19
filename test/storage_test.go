f2bc6fef1d01b9f27d20a244dc8630c706ab6fc6
Jeroen van den Hout <jlvandenhout@gmail.com>
2024-01-19 16:10:53 +0100

    Add a contract and Solo test

---
+++ test/storage_test.go
@@ -0,0 +1,24 @@
+package test
+
+import (
+	"jlvandenhout/storage-test/artifacts"
+	"math/big"
+	"testing"
+
+	"github.com/iotaledger/wasp/packages/solo"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStorageContract(t *testing.T) {
+	env := solo.New(t)
+	chain := env.NewChain()
+	creator, _ := chain.NewEthereumAccountWithL2Funds()
+
+	// deploy solidity `storage` contract, with 42
+	chain.DeployEVMContract(creator, artifacts.StorageAbi, artifacts.StorageBytecode, big.NewInt(0), uint32(42))
+
+	// call EVM contract's `retrieve` view, get 42
+	res, err := chain.CallView("Storage", "retrieve")
+	require.NoError(t, err)
+	t.Log(res)
+}
