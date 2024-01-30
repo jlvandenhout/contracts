38415af620f82f07b768e537be1c1b903676fac7
Jeroen van den Hout <jlvandenhout@gmail.com>
2024-01-30 14:58:57 +0100

    L1 logic

--- contracts/L1Funds.sol
+++ contracts/L1Funds.sol
@@ -3,4 +3,9 @@
 pragma solidity ^0.8.0;
 
 contract L1Funds {
+  function deposit(assets) {
+    msg.caller
+  }
+
+  function withdraw(l1address)
 }
--- test/l1_funds_test.go
+++ test/l1_funds_test.go
@@ -4,10 +4,8 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/iotaledger/wasp/packages/hashing"
 	"github.com/iotaledger/wasp/packages/isc"
 	"github.com/iotaledger/wasp/packages/solo"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -15,10 +13,17 @@ func TestL1Funds(t *testing.T) {
 	env := solo.New(t)
 	chain := env.NewChain()
 	contract := L1Funds.Deploy(chain, nil, big.NewInt(0))
-	keys, address := env.NewKeyPairWithFunds()
+	L1Keys, L1Address := env.NewKeyPairWithFunds()
+	L2Keys, L2Address := solo.NewEthereumAccount()
 
 	metadata := []byte("metadata")
-	nft, _ := env.MintNFTL1(keys, address, metadata)
-	assets := isc.NewAssets(1000, nil, nft)
-	chain.SendFromL1ToL2Account(1000, http.ResponseWriter, r *http.Request
+	nft, _, err := env.MintNFTL1(L1Keys, L1Address, metadata)
+	require.NoError(t, err)
+
+	agentId := isc.NewEthereumAddressAgentID(chain.ChainID, L2Address)
+
+	assets := isc.NewAssets(1000, nil, nft.ID)
+	chain.SendFromL1ToL2Account(0, assets, agentId, L1Keys)
+
+	contract.Call(L2Keys, "deposit", big.NewInt(0), assets)
 }
