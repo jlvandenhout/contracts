687ca03c64a158f8c3f30880e321cd0e4d0ccc05
Jeroen van den Hout <jlvandenhout@gmail.com>
2024-02-19 16:27:11 +0100

    Fix indent

--- contracts/L1Assets.sol
+++ contracts/L1Assets.sol
@@ -5,12 +5,12 @@ pragma solidity ^0.8.0;
 import "@iota/iscmagic/ISC.sol";
 
 contract L1Assets {
-  function withdraw(L1Address memory to) public {
-    ISCAssets memory allowance = ISC.sandbox.getAllowanceFrom(msg.sender);
-    ISC.sandbox.takeAllowedFunds(msg.sender, allowance);
+    function withdraw(L1Address memory to) public {
+        ISCAssets memory allowance = ISC.sandbox.getAllowanceFrom(msg.sender);
+        ISC.sandbox.takeAllowedFunds(msg.sender, allowance);
 
-    ISCSendMetadata memory metadata;
-    ISCSendOptions memory options;
-    ISC.sandbox.send(to, allowance, false, metadata, options);
-  }
+        ISCSendMetadata memory metadata;
+        ISCSendOptions memory options;
+        ISC.sandbox.send(to, allowance, false, metadata, options);
+    }
 }
